e9f74f43f0f8a65894e4c379ddf68f9e25e33892
Nevena Ilieva <118193964+nevenailievaa@users.noreply.github.com>
2025-01-06 13:02:47 +0200

    Add files via upload

---
+++ 00. First Steps In Coding - Lab/03. Rectangle Area/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Въведи страната а:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+int area = a * b;
+
+Console.WriteLine(area);
---
+++ 00. First Steps In Coding - Lab/05. Greeting by Name/Program.cs
@@ -0,0 +1,8 @@
+﻿string name = Console.ReadLine();
+
+//Console.WriteLine($"Hello, {name}!");
+//Console.WriteLine("Hello, " + name + "!");
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.WriteLine("!");
