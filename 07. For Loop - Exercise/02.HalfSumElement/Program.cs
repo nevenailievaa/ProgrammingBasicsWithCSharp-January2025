687247d667bfb0235e2cec63faf5e27728202786
Nevena Ilieva <118193964+nevenailievaa@users.noreply.github.com>
2025-01-26 13:14:37 +0200

    Add files via upload

---
+++ 07. For Loop - Exercise/02.HalfSumElement/Program.cs
@@ -0,0 +1,33 @@
+﻿//Input
+int n = int.Parse(Console.ReadLine());
+
+//Calculations
+int sum = 0;
+int maxNumber = int.MinValue;
+
+for (int i = 1; i <= n; i++)
+{
+    int currentNumber = int.Parse(Console.ReadLine());
+    sum += currentNumber; //sum = sum + currentNumber;
+
+    //Finding the biggest number
+    if (maxNumber < currentNumber)
+    {
+        maxNumber = currentNumber;
+    }
+}
+
+sum -= maxNumber; //sum = sum - maxNumber;
+
+//Output
+if (maxNumber == sum)
+{
+    Console.WriteLine("Yes");
+    Console.WriteLine($"Sum = {sum}");
+}
+else
+{
+    int difference = Math.Abs(maxNumber - sum);
+    Console.WriteLine("No");
+    Console.WriteLine($"Diff = {difference}");
+}
---
+++ 07. For Loop - Exercise/08.TennisRanklist/Program.cs
@@ -0,0 +1,35 @@
+﻿//Input
+int tournamentCount = int.Parse(Console.ReadLine());
+int initialPoints = int.Parse(Console.ReadLine());
+
+//Calculations
+int points = 0;
+int winsCount = 0;
+
+for (int i = 1; i <= tournamentCount; i++)
+{
+    string tournamentResult = Console.ReadLine();
+
+	switch (tournamentResult)
+	{
+		case "W":
+			points += 2000;
+			winsCount++;
+			break;
+		case "F":
+			points += 1200;
+			break;
+		case "SF":
+			points += 720;
+			break;
+	}
+}
+
+double averagePoints = (double)points / tournamentCount;
+points += initialPoints;
+double winPercent = (double)winsCount / tournamentCount * 100;
+
+//Output
+Console.WriteLine($"Final points: {points}");
+Console.WriteLine($"Average points: {Math.Floor(averagePoints)}");
+Console.WriteLine($"{winPercent:f2}%");
