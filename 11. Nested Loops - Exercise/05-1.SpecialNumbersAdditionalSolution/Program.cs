2bdb286c3e7744267a76c67ebb4ff0398c9ac481
Nevena Ilieva <118193964+nevenailievaa@users.noreply.github.com>
2025-02-09 14:12:13 +0200

    Add files via upload

---
+++ 11. Nested Loops - Exercise/05-1.SpecialNumbersAdditionalSolution/Program.cs
@@ -0,0 +1,27 @@
+﻿//Input
+int n = int.Parse(Console.ReadLine());
+
+//Calculations
+for (int currentNum = 1111; currentNum <= 9999; currentNum++)
+{
+    bool isSpecial = true;
+    int num = currentNum;
+
+    for (int i = 1; i <= 4; i++) //Because the digits count of the nums from 1111 to 9999 is always 4
+    {
+        int currentDigit = num % 10;
+        if (currentDigit == 0 || n % currentDigit != 0)
+        {
+            isSpecial = false;
+            break;
+        }
+
+        num = (num - currentDigit) / 10;
+    }
+
+    //Output
+    if (isSpecial)
+    {
+        Console.Write(currentNum + " ");
+    }
+}
---
+++ 11. Nested Loops - Exercise/06.CinemaTickets/Program.cs
@@ -0,0 +1,46 @@
+﻿//Input
+string movieName = "";
+
+//Calculations
+int studentTickets = 0;
+int standardTickets = 0;
+int kidTickets = 0;
+int totalTickets = 0;
+
+while ((movieName = Console.ReadLine()) != "Finish")
+{
+    int seatsCount = int.Parse(Console.ReadLine());
+    string ticketType = "";
+    int soldTickets = 0;
+
+    while ((ticketType = Console.ReadLine()) != "End")
+    {
+        soldTickets++;
+
+        switch (ticketType)
+        {
+            case "student":
+                studentTickets++;
+                break;
+            case "standard":
+                standardTickets++;
+                break;
+            case "kid":
+                kidTickets++;
+                break;
+        }
+
+        if (soldTickets == seatsCount)
+        {
+            break;
+        }
+    }
+
+    totalTickets += soldTickets;
+    Console.WriteLine($"{movieName} - {(double)soldTickets / seatsCount * 100:f2}% full.");
+}
+
+Console.WriteLine($"Total tickets: {totalTickets}");
+Console.WriteLine($"{(double)studentTickets / totalTickets * 100:f2}% student tickets.");
+Console.WriteLine($"{(double)standardTickets / totalTickets * 100:f2}% standard tickets.");
+Console.WriteLine($"{(double)kidTickets / totalTickets * 100:f2}% kids tickets.");
